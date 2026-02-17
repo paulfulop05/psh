1cab5d824a8857d55499036c798f16cc521d0d31
PaulFulop <paul.fulop@stud.ubbcluj.ro>
2026-02-17 17:26:10 +0200

    formatting

--- main.c
+++ main.c
@@ -2,38 +2,36 @@
 #include <stdlib.h>
 
 void psh_run_loop(void);
-char* psh_read_line();
-char** psh_read_args(char* line);
-int psh_execute(char** args);
+char *psh_read_line();
+char **psh_read_args(char *line);
+int psh_execute(char **args);
 
-int main(int argc, char **argv){
+int main(int argc, char **argv) {
   // load config files, if any.
   // ...
 
-
-  //run command loop...
+  // run command loop...
   printf("Hello world!");
-  
-  //perform any shutdown/cleanup...
-  
+
+  // perform any shutdown/cleanup...
+
   return 0;
-  }
+}
 
 // basically the main function -> loop, interpret commands etc
-void psh_run_loop(void){
+void psh_run_loop(void) {
 
   // BASIC LOOP OF A SHELL:
   // 1. read -> read command from the standard input
   // 2. parse -> separate command string into a program and args
   // 3. execute -> run the parsed command
-  
-  char *line; // the string interpretation of the program
+
+  char *line;  // the string interpretation of the program
   char **args; // arguments of the program
-  int status; // status = 0 => exit
-  
-  
-  //more convenient because it executes once before checking the status value
-  do{
+  int status;  // status = 0 => exit
+
+  // more convenient because it executes once before checking the status value
+  do {
     printf("> ");
     line = psh_read_line();
     args = psh_read_args(line);
@@ -41,8 +39,7 @@ void psh_run_loop(void){
 
     free(line);
     free(args);
-  }
-  while(status);
+  } while (status);
 
   return;
 }
