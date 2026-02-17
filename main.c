dd51301f6e5086dc08f12a577f2458b3263a811e
PaulFulop <paul.fulop@stud.ubbcluj.ro>
2026-02-17 18:36:42 +0200

    implementing readargs

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void psh_run_loop(void);
-char *psh_read_line(void);
+char *psh_read_line(
+    void); // TODO switch to a better implementation -> getline or something.
 char **psh_read_args(char *line);
 int psh_execute(char **args);
 
@@ -48,10 +50,11 @@ char *psh_read_line(void) {
   int bufsize = PSH_RL_BUFSIZE;
   int position = -1;
   char *buffer = malloc(sizeof(char) * bufsize);
-  int c = getchar(); // current character
+  int c = getchar(); // current character (using int because I need to check EOF
+                     // as well, which is an integer)
 
   if (!buffer) {
-    fprintf(stderr, "psh: some alloation error occured.");
+    fprintf(stderr, "psh: some alloation error occured.\n");
     exit(EXIT_FAILURE);
   }
 
@@ -62,17 +65,51 @@ char *psh_read_line(void) {
 
     if (position >= bufsize) {
       bufsize += PSH_RL_BUFSIZE;
-      buffer = realloc(buffer, bufsize);
+      buffer = realloc(buffer, sizeof(char) * bufsize);
 
       if (!buffer) {
-        fprintf(stderr, "psh: some alloation error occured.");
+        fprintf(stderr, "psh: some alloation error occured.\n");
         exit(EXIT_FAILURE);
       }
     }
 
     buffer[++position] = c;
   }
-  
+
   buffer[++position] = '\0';
   return buffer; // no need to deallocate, that happens in the loop function
+}
+
+#define PSH_TOKENS_BUFSIZE 64
+#define PSH_TOKEN_DELIMITER " \t\r\n\a"
+// tokenizes the input according to a delimiter (a space in my case, for simplicity)
+char **psh_read_args(char *line) {
+  int bufsize = PSH_TOKENS_BUFSIZE, position = -1;
+  char **tokens = malloc(sizeof(char *) * bufsize);
+  char *token = strtok(line, PSH_TOKEN_DELIMITER);
+
+  if (!tokens) {
+    fprintf(stderr, "psh: some allocation error occured\n");
+    exit(EXIT_FAILURE);
+  }
+
+  while (token != NULL) {
+    tokens[++position] = token;
+
+    if (position >= bufsize) // we have more than 64 args by any chance
+    {
+      bufsize += PSH_RL_BUFSIZE;
+      tokens = realloc(tokens, sizeof(char*) * bufsize);
+
+      if (!tokens) {
+        fprintf(stderr, "psh: some allocation error occured\n");
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    token = strtok(NULL, PSH_TOKEN_DELIMITER);
+  }
+
+  tokens[++position] = NULL;
+  return tokens;
 }
